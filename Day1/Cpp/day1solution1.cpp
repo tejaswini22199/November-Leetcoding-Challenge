1f98923840b91185701140dc15869df013e12192
tejaswini22199 <tejaswini22199@gmail.com>
2020-11-02 20:51:46 +0530

    Modified day 1

--- Day1/Cpp/day1solution1.cpp
+++ Day1/Cpp/day1solution1.cpp
@@ -1,3 +1,8 @@
+//Question
+/*Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1.
+The linked list holds the binary representation of a number.
+Return the decimal value of the number in the linked list.*/
+
 class Solution {
      int length=0;
 public:
